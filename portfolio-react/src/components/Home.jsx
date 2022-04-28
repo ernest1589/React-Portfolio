9b86a7a87f929b939fd0c0e4ba5b237be990a0fb
ernest1589 <87840643+ernest1589@users.noreply.github.com>
2022-04-28 05:43:34 -0500

    add about complete home

--- portfolio-react/src/components/Home.jsx
+++ portfolio-react/src/components/Home.jsx
@@ -1,8 +1,34 @@
 import React from "react";
-import { HiArrowNarrowRight } from "react-icons";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
-  return <div className="w-full h- bg-[0a192f]"></div>;
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-red-600">Hi my Name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Ernesto Ramirez
+        </h1>
+        <h2 className="text-4xl sm:text-7xl text-[#8892b0]">
+          I am a Full Stack Developer
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I'm a currently student of a full-stack developer program, where I am
+          learning everything that is required to be one, using different
+          technologies. I will specialize in back end technologies. My favorite
+          part of being a developer it is that the learning never ends.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-400 hover:border-red-400">
+            View Work
+            <span className="grou-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- portfolio-react/src/components/Navbar.jsx
+++ portfolio-react/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
-import { HiOutlinemAil } from "react-icons/hi";
+import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/LOGO.png";
 
@@ -39,20 +39,44 @@ const Navbar = () => {
         <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">Skills</li>
-        <li className="py-6 text-4xl">WOrk</li>
+        <li className="py-6 text-4xl">Work</li>
         <li className="py-6 text-4xl">Contact</li>
       </ul>
 
       {/*SOcial Icons */}
 
-      <div calssName="hidden lg:flex fixed flex-col top-[35%] left-0">
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px]">
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
               href="/"
             >
               Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
         </ul>
